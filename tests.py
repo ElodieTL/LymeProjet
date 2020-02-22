9cdca93da71df550cfea11c8429245ec4a211dd4
Gabrielle <gabrielle.lajeunesse-langdeau.1@ulaval.ca>
2020-02-22 12:17:36 -0500

    test gab

--- tests.py
+++ tests.py
@@ -15,27 +15,32 @@ import matplotlib.pyplot as plt
 
 # Fonction permettant d'extraire le code EPSG (la projection) d'une donnée vectorielle.
 # data: objet geopandas représentant la donnée vectorielle.
-# dataCRS: entier représentant le code EPSG de la projection utilisée.
+# dataCRS: entier représentant le code EPSG de la projection utilisée (#).
 # dataCRSStr: String représentant le code EPSG de la projection utilisée (EPSG:#).
 def extractEPSGVector(data):
     # Extraire le code EPSG (la projection) de la zone d'intérêt.
     dataCRS = re.search('epsg:(.*)', data.crs["init"])
 
-    # Si une projection existe, extraire le code et un String pour d'autres fonctions.
-    if dataCRS:
+    # Si une projection existe, extraire le code numérique et un String pour d'autres fonctions.
+    if dataCRS is not None:
         dataCRS = dataCRS.group(1)
         dataCRSStr = "EPSG:" + dataCRS
 
         return dataCRS, dataCRSStr
 
-    return None, None
+    # Si une projection n'est pas détectée, retourner None.
+    else:
+        return None, None
 
 # Fonction permettant d'extraire le code EPSG (la projection) de données de type raster.
 # inPath: String représentant le chemin vers le fichier raster entrant.
-# rasterCRS: entier représentant le code EPSG de la projection utilisée.
+# rasterCRS: entier représentant le code EPSG de la projection utilisée (#).
 # rasterCRSStr: String représentant le code EPSG de la projection utilisée (EPSG:#).
 def extractEPSGRaster(inPath):
+    # Ouvrir le raster.
     raster = gdal.Open(inPath)
+
+    # Extraire le code EPSG.
     rasterCRS = osr.SpatialReference(wkt = raster.GetProjection()).GetAttrValue('AUTHORITY', 1)
     rasterCRSStr = "EPSG:" + rasterCRS
 
@@ -43,13 +48,15 @@ def extractEPSGRaster(inPath):
 
 # Fonction permettant de créer une liste de fichiers de type raster.
 # inDir: String représentant le répertoire où sont stockées les fichiers.
-# extension: String ou tuple représentant le(s) extension(s) à rechercher.
 # rasterFiles: List contenant les fichiers de type raster.
-def listRasterFiles(inDir, extension):
+def listRasterFiles(inDir):
+    # Créer un liste vide.
     rasterFiles = []
+
+    # Parcourir le répertoire afin de lister les fichiers de type raster.
     for root, dirs, files in os.walk(inDir):
         for file in files:
-            if file.endswith(extension):
+            if file.endswith((".tif", ".TIF", ".TIFF", ".tiff")):
                 rasterFiles.append(os.path.join(root, file))
 
     return rasterFiles
@@ -68,15 +75,18 @@ def downloadData(url, outPath):
 # outPath: String représentant le chemin vers le fichier raster sortant.
 # dstCrs: String représentant le code EPSG de la projection voulue (EPSG:#).
 def reprojectRaster(inPath, outPath, dstCRS):
-    dst_crs = dstCRS
     fileName = os.path.basename(inPath)
     print("Reprojecting raster " + fileName + "...")
 
+    dst_crs = dstCRS
+
+    # Extraire les métadonnées.
     with rio.open(inPath) as src:
         transform, width, height = calculate_default_transform(src.crs, dst_crs, src.width, src.height, *src.bounds)
         kwargs = src.meta.copy()
         kwargs.update({'crs': dst_crs, 'transform': transform, 'width': width, 'height': height})
 
+        # Effectuer la reprojection et exporter le raster résultant.
         with rio.open(outPath, 'w', **kwargs) as dst:
             for i in range(1, src.count + 1):
                 reproject(source = rio.band(src, i), destination = rio.band(dst, i), src_transform = src.transform, src_crs = src.crs, dst_transform = transform, dst_crs = dst_crs, resampling = Resampling.nearest)
@@ -85,8 +95,13 @@ def reprojectRaster(inPath, outPath, dstCRS):
 # inPath: String représentant le chemin vers le fichier raster entrant.
 # rasters: List de rasters ouverts.
 def addRastertoRasters(inPath, rasters):
+    # Ouvrir le raster.
     raster = rio.open(inPath)
+
+    # Ajouter à la liste.
     rasters.append(raster)
+
+    return rasters
 
 # Fonction permettant de transformer une géométrie d'une geodataframe en format json.
 # gdf: objet de type geodataframe contenant un champ 'geometry'.
@@ -94,18 +109,25 @@ def geoToJson(gdf):
     return[json.loads(gdf.to_json())['features'][0]['geometry']]
 
 # Fonction permettant de découper un raster selon une région d'intérêt.
-# inRaster: String représentant le chemin vers le fichier raster entrant.
+# inPath: String représentant le chemin vers le fichier raster entrant.
+# outPath: String représentant le chemin vers le fichier raster sortant.
 # clipPoly: objet de type json représentant le polygone servant au découpage.
-def clipRaster(inPath, outPath, clipPoly, epsgPoly):
+# epsgROI: int représentant le code EPSG de la projection voulue (#).
+def clipRaster(inPath, outPath, clipPoly, epsgROI):
     fileName = os.path.basename(inPath)
     print("Clipping raster " + fileName + "...")
 
+    # Ouvrir le raster.
     raster = rio.open(inPath)
+
+    # Découper le raster
     outRaster, outTransform = mask(dataset = raster, shapes = clipPoly, crop = True)
+
+    # Mettre à jour les métadonnées.
     outMeta = raster.meta.copy()
+    outMeta.update({"driver": "GTiff", "height": outRaster.shape[1], "width": outRaster.shape[2], "transform": outTransform, "crs": pycrs.parse.from_epsg_code(epsgROI).to_proj4()})
 
-    outMeta.update({"driver": "GTiff", "height": outRaster.shape[1], "width": outRaster.shape[2], "transform": outTransform, "crs": pycrs.parse.from_epsg_code(epsgPoly).to_proj4()})
-
+    # Exporter le raster résultant.
     with rio.open(outPath, "w", **outMeta) as dest:
         dest.write(outRaster)
 
@@ -114,35 +136,48 @@ def clipRaster(inPath, outPath, clipPoly, epsgPoly):
 # width: largeur d'un pixel.
 # height: hauteur d'un pixel.
 def getPixelSize(inPath):
+    # Ouvir le raster
     raster = rio.open(inPath)
+
+    # Extraire la largeur et la hauteur d'un pixel.
     width = raster.transform[0]
     height = -(raster.transform[4])
 
     return width, height
 
-#
-def resampleRaster(inPath, outPath, width, pixelSize):
+# Fonction permettant de rééchantillonner un raster.
+# inPath: String représentant le chemin vers le fichier raster entrant.
+# outPath: String représentant le chemin vers le fichier raster sortant.
+# inPixelSize: largeur/hauteur d'un pixel déterminée à l'aide de la fonction getPixelSize().
+# outPixelSize: largeur/hauteur d'un pixel voulue.
+def resampleRaster(inPath, outPath, inPixelSize, outPixelSize):
     fileName = os.path.basename(inPath)
     print("Resampling raster " + fileName + "...")
 
-    factor = width / pixelSize
+    # Calcul du facteur d'agrandissement/de réduction.
+    factor = inPixelSize / outPixelSize
 
+    # Ouvrir le raster.
     raster = rio.open(inPath)
-    meta = raster.meta.copy()
 
+    # Rééchantillonner le raster.
     data = raster.read(out_shape = (raster.count, int(raster.width * factor), int(raster.height * factor)), resampling = Resampling.bilinear)
     transform = raster.transform * raster.transform.scale((raster.width / data.shape[-2]), (raster.height / data.shape[-1]))
+
+    # Mettre à jour les métadonnées.
+    meta = raster.meta.copy()
     meta.update({"driver": "GTiff", "height": int(raster.height * factor), "width": int(raster.width * factor), "transform": transform})
 
+    # Exporter le raster résultant.
     with rio.open(outPath, 'w', **meta) as dst:
         dst.write(data)
 
 def main():
-    # Dimension d'un pixel.
+    # Dimension d'un pixel pour un raster.
     pixelSize = 30
 
     # Importer et lire les données du shapelefile représentant la zone d'intérêt.
-    ROIPath = "Z:/MALAM357/GMT-3051 Projet en génie géomatique II/LymeProjet/ROI/ROI_Projet_Genie_Maladies_Vectorielles_v2.shp"
+    ROIPath = "Z:\GALAL35\Projet_lyme\LymeProjet\ROI\ROI_Projet_Genie_Maladies_Vectorielles_v2.shp"
     ROIData = gpd.read_file(ROIPath)
 
     # Transformer en format json
@@ -155,7 +190,7 @@ def main():
     rasters = [] # Créer une liste vide qui contiendra les rasters téléchargés.
 
     # Répertoire où les données seront enregistrées.
-    foretsDir = r"Z:\MALAM357\GMT-3051 Projet en génie géomatique II\Données\Forêt"
+    foretsDir = r"Z:\GALAL35\Projet_lyme\Données\Forêt"
 
     # Liste de liens menant aux données.
     urlList = [
