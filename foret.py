d54799e4d35a10fecc0bfc0d69b4a1ec13c337ac
Maxime Lamer <maxlamer01@hotmail.com>
2020-03-06 12:51:56 -0500

    updates foret.py

--- foret.py
+++ foret.py
@@ -1,47 +1,31 @@
-#
-
-import os
+from generic import *
 import numpy as np
-from rasterio.plot import show
-import re
-import osr
-import urllib
-import geopandas as gpd
-import rasterio as rio
-import json
-import matplotlib.pyplot as plt
-import pycrs
-from rasterio.merge import merge
-from rasterio.mask import mask
-from rasterio.plot import show
-from rasterio.warp import calculate_default_transform, reproject, Resampling
-import gdal
 import earthpy.spatial as es
 import earthpy.plot as ep
 import cv2
 
-# Data dir
-data_dir = "X:\ELTAL8\ProjetLYME\ROI_Projet_Genie_Maladies_Vectorielles_v2\Données\Forêt"
+# Répertoire contenant les données de forêt.
+# dir = "X:\ELTAL8\ProjetLYME\ROI_Projet_Genie_Maladies_Vectorielles_v2\Données\Forêt"
+dir = r"Z:\MALAM357\GMT-3051 Projet en génie géomatique II\Donnees\Foret"
 
 # Filepath
-fpfeuillu = os.path.join(data_dir, "NFI_MODIS250m_2011_kNN_SpeciesGroups_Broadleaf_Spp_v1_clip.tif")
-fpconifere = os.path.join(data_dir, "NFI_MODIS250m_2011_kNN_SpeciesGroups_Needleleaf_Spp_v1_clip.tif")
-fpunknow = os.path.join(data_dir, "NFI_MODIS250m_2011_kNN_SpeciesGroups_Unknown_Spp_v1_clip.tif")
+pathFeuillus = os.path.join(dir, "NFI_MODIS250m_2011_kNN_SpeciesGroups_Broadleaf_Spp_v1_clip.tif")
+pathConiferes = os.path.join(dir, "NFI_MODIS250m_2011_kNN_SpeciesGroups_Needleleaf_Spp_v1_clip.tif")
+pathUnknown = os.path.join(dir, "NFI_MODIS250m_2011_kNN_SpeciesGroups_Unknown_Spp_v1_clip.tif")
+pathAll = os.path.join(dir, "NFI_MODIS250m_2011_kNN_SpeciesGroups_Unknown_Spp_v1_clip.tif")
 
-# Liste de raster
-file_list = [fpfeuillu, fpconifere, fpunknow]
+# Liste des fichiers raster pertinents.
+liste = [pathFeuillus, pathConiferes, pathUnknown]
 
-# Fusionner les rasters
-es.stack(file_list, "X:\ELTAL8/foret.tif")
+# Fusionner les rasters contenus dans la liste.
+es.stack(liste, pathAll)
 
 # bande 0 = inconnu bande 1 = feuillu bande 2 = conifère
 
-img_file = 'X:\ELTAL8/foret.tif'
-img = cv2.imread(img_file, cv2.IMREAD_UNCHANGED)           # rgb
+img = cv2.imread(pathAll, cv2.IMREAD_UNCHANGED)           # rgb
 rows, cols = img.shape[:2]
 
-img2 = np.ones((rows, cols,1),np.uint8)*255
-
+img2 = np.ones((rows, cols,1), np.uint8) * 255
 
 for i in range(rows):
     for j in range(cols):
@@ -49,23 +33,18 @@ for i in range(rows):
             img2[i,j] = 0
 
         if (img[i,j,0] > 50):
-                img2[i, j] = 0
+            img2[i,j] = 0
 
         elif (img[i,j,2] > 0 and img[i,j,1] / img[i,j,2] > 0.7 and img[i,j,1] / img[i,j,2] < 1.3):
             img2[i,j] = 75
 
         elif (img[i,j,2] < img[i,j,1]):
-             img2[i, j] = 200
+            img2[i,j] = 200
 
         elif (img[i,j,2] > img[i,j,1]):
-             img2[i, j] = 125
-
-
+            img2[i,j] = 125
 
 # Feuillu = 200 conifère = 125 pas foret = 0 mixte = 75
 
-
-window_name = 'image'
-cv2.imshow(window_name, img2)
-
-cv2.waitKey(0)
+cv2.imshow('image', img2)
+cv2.waitKey()
